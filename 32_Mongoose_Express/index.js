30d4b7073f291338042c95a280b8910f00071c65
Manh Tran <manhnus@gmail.com>
2022-07-10 22:32:03 +0700

    done 403-creating products

--- 32_Mongoose_Express/index.js
+++ 32_Mongoose_Express/index.js
@@ -16,10 +16,28 @@ mongoose.connect('mongodb://localhost:27017/farmStand', {})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: true}))
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
     res.render('products/index', { products })
+})
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+app.post('/products', async (req, res) => {
+    const product = new Product(req.body)
+    await product.save()
+    res.redirect(`/products/${product._id}`)
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params
+    console.log(req.params) 
+    const product = await Product.findById(id)
+    res.render('products/show', { product })
 })
 
 app.listen(3000, () => {
