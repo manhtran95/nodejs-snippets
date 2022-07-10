511fc6dae082690af06bcd11aee03ee408223669
Manh Tran <manhnus@gmail.com>
2022-07-10 21:27:15 +0700

    401-Products Index

---
+++ 32_Mongoose_Express/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const path = require('path')
+
+const Product = require('./models/product')
+
+mongoose.connect('mongodb://localhost:27017/farmStand', {})
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR")
+        console.log(err)
+    })
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', { products })
+})
+
+app.listen(3000, () => {
+    console.log("APP IS LISTENING ON PORT 3000")
+})
