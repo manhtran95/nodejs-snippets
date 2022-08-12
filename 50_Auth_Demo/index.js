c787775c0cf3d3117ae3b38e72ffb29264d23e37
nndhuy <nndhuy@cmcglobal.vn>
2022-08-12 14:50:05 +0700

    log out

--- 50_Auth_Demo/index.js
+++ 50_Auth_Demo/index.js
@@ -55,11 +55,16 @@ app.post('/login', async (req, res) => {
     }
 })
 
+app.post('/logout', (req, res) => {
+    req.session.user_id = null
+    res.redirect('/login')
+})
+
 app.get('/secret', (req, res) => {
     if (!req.session.user_id){
         return res.redirect('/login')
     }
-    res.send('THIS IS SECRET! YOU SHOULD NOT SEE ME IF NOT LOGGED IN!')
+    res.render('secret')
 })
 
 app.listen(3000, () => {
