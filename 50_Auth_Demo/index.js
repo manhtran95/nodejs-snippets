07da8409b32631de9d11cbe6fd5e30c4e5d2fece
nndhuy <nndhuy@cmcglobal.vn>
2022-08-12 10:10:37 +0700

    auth demo - log in

--- 50_Auth_Demo/index.js
+++ 50_Auth_Demo/index.js
@@ -36,6 +36,21 @@ app.post('/register', async (req, res) => {
     res.redirect('/')
 })
 
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body
+    const user = await User.findOne({ username })
+    const validPassword = await bcrypt.compare(password, user.password)
+    if (validPassword) {
+        res.send('Yay welcome!')
+    } else {
+        res.send('Try again')
+    }
+})
+
 app.get('/secret', (req, res) => {
     res.send('THIS IS SECRET! YOU SHOULD NOT SEE ME IF NOT LOGGED IN!')
 })
